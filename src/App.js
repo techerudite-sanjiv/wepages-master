bb788ca9965db35fc2cb36bdc796e7619525b1ed
Sanjeev Kumar <sanjiv@ttecherudite.com>
2023-03-01 15:45:42 +0530

    updated latest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from "react";
-import { bookings } from "./constants";
-import Table from "./Scheduler/Table";
+
 import Table1 from "./Scheduler/Table1";
-import SingleUser from "./SingleUser";
-import Today from "./Today";
-import './App.css'
-import Abc from "./Abc";
+
+
 import Table2 from "./Scheduler/Table2";
 const App = () => {
   return (
@@ -14,19 +10,10 @@ const App = () => {
         <div>
           <Table1 />
         </div>
-        {/* <div style={{ display: "flex", justifyContent: "space-between" }}>
-          {bookings.map((item, index) => {
-            return <SingleUser {...item} key={index} />;
-          })}
-        </div> */}
-       <div>
-       {/* <Table2/> */}
-       </div>
+        <div>
+          <Table2 />
+        </div>
       </div>
-      <Today/>
-    {/* <Abc/> */}
-      {/* <Table1/> */}
-      {/* <Table/> */}
     </>
   );
 };
